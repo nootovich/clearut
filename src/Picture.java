b72ae3e91776e8362fba424bc8aed03c599ea133
nootovich <mailnonamer@gmail.com>
2023-09-12 14:35:58 +0300

    Added use of buffered image to Pictures with "RECTANGLE" mode

--- src/Picture.java
+++ src/Picture.java
@@ -7,14 +7,14 @@ public class Picture extends Child {
 
     public int x, y, w, h;
     public boolean visible = true, hovered, active;
-    public String      filepath = "";
-    public Image       image    = null;
-    private BufferedImage bufImage;
-    public PictureType type     = PictureType.RECTANGLE;
+    public  String        filepath = "";
+    public  Image         image    = null;
+    private BufferedImage buffImg;
+    public  PictureType   type     = PictureType.RECTANGLE;
 
     public Picture(int x, int y, int w, int h, int z, String filepath) {
         this.x = x; this.y = y; this.w = w; this.h = h; this.z = z;
-        change(filepath);
+        setImageFromFile(filepath);
     }
 
     public Picture(int x, int y, int w, int h, int z, String filepath, String name) {
@@ -37,28 +37,37 @@ public class Picture extends Child {
         if (visible) {
             switch (type) {
                 case RECTANGLE -> {
-                    g2d.drawImage(image, x, y, w, h, null);
-                    // TODO: this is temporary
-                    if (active) {
-                        g2d.setColor(new Color(0x30000000, true));
-                        g2d.fillRect(x, y, w, h);
-                    } else if (hovered) {
-                        g2d.setColor(new Color(0x20ffffff, true));
-                        g2d.fillRect(x, y, w, h);
+                    if (buffImg == null) {
+                        buffImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+                        Graphics2D big2d = buffImg.createGraphics();
+                        big2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                        big2d.drawImage(image, 0, 0, w, h, null);
+                        big2d.dispose();
+                    }
+                    g2d.drawImage(buffImg, x, y, w, h, null);
+
+                    { // TODO: this is temporary
+                        if (active) {
+                            g2d.setColor(new Color(0x30000000, true));
+                            g2d.fillRect(x, y, w, h);
+                        } else if (hovered) {
+                            g2d.setColor(new Color(0x20ffffff, true));
+                            g2d.fillRect(x, y, w, h);
+                        }
                     }
                 }
                 case CIRCLE -> {
-                    if (bufImage == null) {
-                        bufImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-                        Graphics2D big2d = bufImage.createGraphics();
+                    if (buffImg == null) {
+                        buffImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+                        Graphics2D big2d = buffImg.createGraphics();
                         big2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                        big2d.fillOval(0,0, bufImage.getWidth(), bufImage.getHeight());
+                        big2d.fillOval(0, 0, buffImg.getWidth(), buffImg.getHeight());
                         AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_IN);
                         big2d.setComposite(ac);
-                        big2d.drawImage(image, 0, 0, bufImage.getWidth(), bufImage.getHeight(), null);
+                        big2d.drawImage(image, 0, 0, w, h, null);
                         big2d.dispose();
                     }
-                      g2d.drawImage(bufImage, x, y, w, h, null);
+                    g2d.drawImage(buffImg, x, y, w, h, null);
                 }
                 default -> System.out.println("Not implemented image type!");
             }
@@ -67,7 +76,7 @@ public class Picture extends Child {
     }
 
     // TODO: rename function
-    public void change(String filepath) {
+    public void setImageFromFile(String filepath) {
         try {
             this.image    = ImageIO.read(new File(filepath));
             this.filepath = filepath;
